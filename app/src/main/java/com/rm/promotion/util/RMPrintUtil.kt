e42e1f36134363d0945da11d78b2f97051005d4f
panya.n <panya.naknoppakun@gmail.com>
2022-12-09 16:44:25 +0700

    Update font size

--- app/src/main/java/com/rm/promotion/util/RMPrintUtil.kt
+++ app/src/main/java/com/rm/promotion/util/RMPrintUtil.kt
@@ -311,7 +311,7 @@ class RMPrintUtil {
                     if ("1".equals(detail.type)) {
                         fontSize = detail.text_font.toFloat()
                         SunmiPrintHelper.getInstance()
-                            .printWithSize(detail.text_detail, fontSize * 1.5f)
+                            .printWithSize(detail.text_detail, fontSize * 1.4f)
                     }
                 }
             } else {
@@ -412,7 +412,7 @@ class RMPrintUtil {
                     if ("0".equals(detail.type)) {
                         fontSize = detail.text_font.toFloat()
                         SunmiPrintHelper.getInstance()
-                            .printWithSize(detail.text_detail, fontSize * 1.6f)
+                            .printWithSize(detail.text_detail, fontSize * 1.4f)
                     }
                 }
                 SunmiPrintHelper.getInstance().printTitle("\nจำนวน $promotionTotal สิทธิ์")
@@ -448,7 +448,7 @@ class RMPrintUtil {
                     if ("1".equals(detail.type)) {
                         fontSize = detail.text_font.toFloat()
                         SunmiPrintHelper.getInstance()
-                            .printWithSize(detail.text_detail, fontSize * 1.5f)
+                            .printWithSize(detail.text_detail, fontSize * 1.4f)
                     }
                 }
             } else {
